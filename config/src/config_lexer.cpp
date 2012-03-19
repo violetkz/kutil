f1470c299b4fd32438562892a44ca5c9ab38ece7
Ken <proxy.vk@gmail.com>
2012-03-19 23:46:02 +0800

    try some codes

--- config/src/config_lexer.cpp
+++ config/src/config_lexer.cpp
@@ -30,8 +30,8 @@ bool strcmp_ncase(const std::string& _f1, const std::string& _f2)
     std::string _f1_lower;
     std::string _f2_lower;
 
-    _f1_lower.reserve(len);
-    _f2_lower.reserve(len);
+    //_f1_lower.reserve(len);
+    //_f2_lower.reserve(len);
 
     std::transform(_f1.begin(), _f1.end(), _f1_lower.begin(), ::tolower);
     std::transform(_f2.begin(), _f2.end(), _f2_lower.begin(), ::tolower);
