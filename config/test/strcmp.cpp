d9212bc28b1b1b9136eedf41f6076c2338590b51
ken <proxy.vk@gmail.com>
2012-03-20 14:47:56 +0800

    fix some bugs

--- config/test/strcmp.cpp
+++ config/test/strcmp.cpp
@@ -27,8 +27,8 @@ bool strcmp_ncase(const std::string& _f1, const std::string& _f2)
     if (_f1 == _f2){
         std::cout<< __func__ << "f1==f2: ==" << std::endl;
     }
-    std::string _f1_lower;
-    std::string _f2_lower;
+    std::string _f1_lower('0x00', len); 
+    std::string _f2_lower('0x00', len);
 
     //_f1_lower.reserve(len);
     //_f2_lower.reserve(len);
