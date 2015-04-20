2c341cedd089c1117eca1ff41dcbe18a31c0c650
ken <proxy.vk@gmail.com>
2015-04-20 17:52:52 +0800

    test code

--- miniawk/ns.y
+++ miniawk/ns.y
@@ -14,6 +14,13 @@ pattern: /* empty */ {$$ = NULL;}
     | str       {$$ = $1;}
 
 action:  /* empty */ {$$ = NULL;}
-    | func_exp  {$$ = node_new(func_exp_node, $1, NULL); }
-    
+    | func_exp  {$$ = $1; }
+
+func_exp:  func_name '(' param_list ')'  { $$ = builtin_func_node(func_name, $3); }
+
+param_list:  /* empty */ {$$ = NULL; }
+    | param  { $$ = func_paramter_list($1); }
+    | param_list ',' param  { $$ = append_param($1, $2); };
+
 %%
+
--- miniawk/ns_def.h
+++ miniawk/ns_def.h
@@ -1,8 +1,27 @@
 
-struct ast {
+
+struct node {
     int node_type;
-    struct ast *left;
-    struct ast *right;
+    node(int t) : node_type(t) {}
 };
 
+struct stmt_node : public node {
+    node* pattern;
+    node* action;
+    stmt_node(node* p, node* act):node(STMT_NODE), pattern(p), action(act) {}
+};
 
+struct builtin_func_node : public node {
+    char* func_name;
+    //std::list<char *> plist;
+    node* plist;
+    builtin_func_node(char *name, node *plist)
+            :node(func_node),
+            func_name(name),
+            plist(plist) {}
+}
+
+struct func_paramter_list : public node {
+    std::list<char* > plist;
+    func_paramter_list(char *node);
+}
