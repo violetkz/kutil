404fca62e8b22aedfb9fbef69c6a1b85a547d01f
Ken Chow <violet.kz@gmail.com>
2015-04-21 22:24:52 +0800

    add test code

--- miniawk/ns_def.h
+++ miniawk/ns_def.h
@@ -12,6 +12,21 @@ struct node {
     node(int t) : node_type(t) {}
 };
 
+struct str_node : public node {
+    char* s;
+    str_node(char* str):node(STR_NODE), s(str) {
+        /* do nothing */
+    }
+};
+
+struct regex_str_node : public node {
+    char* regex_str;
+    
+    regex_str_node(char* str):node(REGEX_STR_NODE), regex_str(str) {
+        /* do nothing */  
+    }
+}
+
 struct stmt_node : public node {
     node* pattern;
     node* action;
@@ -33,17 +48,17 @@ struct builtin_func_node : public node {
             plist(plist) {
         /* do nothing */
     }
-}
+};
 
 struct func_paramter_list : public node {
     std::list<char* > plist;
     func_paramter_list():node(FUNC_PARAM_LIST_NODE), plist(){
         /* do nothing */
     }
-}
+};
 
 func_paramter_list* append_param(func_paramter_list *plist, const char* new_param) {
     if (plist && new_param) 
-        plist->push_back(new_param);
+        plist.push_back(new_param);
     return plist;
 }
