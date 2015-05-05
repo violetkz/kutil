68c367cf5fd0e6e430cdf73cd7a13442f0bb76c5
Ken Chow <violet.kz@gmail.com>
2015-05-06 00:38:13 +0800

    go on

--- miniawk/ns_string.h
+++ miniawk/ns_string.h
@@ -6,11 +6,14 @@ class ns_string {
 public:
     ns_string(char *s);
     ~ns_string();
+
+    cont char *c_str();
     
     ns_string(const ns_string &s);
     ns_string &operator = (const ns_string &s);
 private:
     char *str_ptr;
+    int  *ref_count;
 };
 
 #endif //~ns_string__h__
