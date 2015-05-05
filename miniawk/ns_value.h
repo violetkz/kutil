da676833aa501f1f6785aacd6bfa6f54167432f0
ken <proxy.vk@gmail.com>
2015-05-05 12:52:35 +0800

    go on

--- miniawk/ns_value.h
+++ miniawk/ns_value.h
@@ -1,15 +1,15 @@
 #ifndef ns_value_h___
 #define ns_value_h___
 
+enum ns_value_type {
+    NSVAL_INTEGER,          /* integer */
+    NSVAL_LITERAL_STR,      /* literal string */
+    NSVAL_BOOLEAN,          /* boolean */
+    NSVAL_EXPERESS_AST,     /* expression  */
+    NSVAL_UNINITIALIZED,    /* un-initialized */
+    NSVAL_ILLEGAL           /* illegal */
+};
 struct ns_value {
-    enum ns_value_type {
-        NSVAL_INTEGER,      /* integer */
-        NSVAL_LITERAL_STR,  /* literal string */
-        NSVAL_BOOLEAN,      /* boolean */
-        NSVAL_EXPERESS_AST, /* expression  */
-        NSVAL_UNINITITAL,   /* uninitital */
-        NSVAL_ILLEGAL       /* illegal */
-    };
     ns_value_type type;
     union {
         int     int_val;
@@ -18,11 +18,11 @@ struct ns_value {
         node    *node_val;
     };
     
-    ns_value() : type(NSVAL_UNINITITAL), int_val(0) {}
+    ns_value() : type(NSVAL_UNINITIALIZED), int_val(0) {}
     ns_value(int v) : type(NSVAL_INTEGER), int_val(v) {}
     ns_value(bool b) : type(NSVAL_BOOLEAN), bool_val(b) {}
     ns_value(char *s) : type(NSVAL_LITERAL_STR), chr_val(s) {}
-    
+    ns_value(ns_value_type t) : type(t), int_val(0) {}
 };
 
 /* helper function.  
@@ -35,10 +35,10 @@ ns_value make_illegal_value {
     return illegal;
 }
 
-ns_value operator+ (const ns_value &l, const ns_value &r);
-ns_value operator- (const ns_value &l, const ns_value &r);
-ns_value operator* (const ns_value &l, const ns_value &r);
-ns_value operator/ (const ns_value &l, const ns_value &r);
+const ns_value operator+ (const ns_value &l, const ns_value &r);
+const ns_value operator- (const ns_value &l, const ns_value &r);
+const ns_value operator* (const ns_value &l, const ns_value &r);
+const ns_value operator/ (const ns_value &l, const ns_value &r);
 
 
 #endif  //~ns_value_h___
